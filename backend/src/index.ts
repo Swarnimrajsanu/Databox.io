66761b01b2a32fbba76306c5820d1a425ff37dc3
Swarnimrajsanu <149811757+Swarnimrajsanu@users.noreply.github.com>
2026-02-07 01:43:21 +0530

    feat: add AWS S3 integration for presigned URLs and implement JWT authentication middleware
    - Updated package.json to include AWS SDK dependencies and ts-node for TypeScript execution.
    - Modified Prisma schema to reorder fields and ensure proper relationships between models.
    - Created a new config file for JWT secrets and total decimals.
    - Enhanced server startup message in index.ts for better visibility.
    - Implemented authMiddleware for JWT verification and user identification.
    - Added a new route in user router to generate presigned URLs for S3 uploads.
    - Cleaned up tsconfig.json by removing unnecessary newline.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,4 +10,6 @@ const app = express();
 app.use("/v1/user", userRouter);
 app.use("/v1/worker", workerRouter);
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -1,14 +1,60 @@
+import { S3Client, } from "@aws-sdk/client-s3";
+import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 import { PrismaClient } from '@prisma/client';
+import "dotenv/config";
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
+import { authMiddleware } from '../middleware.js';
 
-const JWT_SECRET = "your_jwt_secret_key"
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+
+
+const JWT_SECRET = "billubadmosh"
 
 const router = Router();
 
 const prisma = new PrismaClient({});
 
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 
+
+router.get("/presignedUrl", authMiddleware, async (req,res) => { 
+    const userId = req.userId;
+
+    const { url, fields } = await createPresignedPost(s3Client, {
+        Bucket: 'task-to-reward',
+        Key: `thumbnails/${userId}/${Math.random()}/image.jpg`,
+        Conditions: [
+          ['content-length-range', 0, 5 * 1024 * 1024] // 5 MB max
+        ],
+        Fields: {
+            success_action_status: '201',
+            'Content-Type': 'image/jpeg'
+        },
+        Expires: 3600
+    })
+
+    console.log(url, fields)
+
+    res.json({
+        preSignedUrl: url,
+    })
+
+    
+})
 
 //sigin with wallet
 //signing a message
@@ -44,3 +90,4 @@ router.post("/signin", async (req, res) => {
     });
 
 export default router;
+
