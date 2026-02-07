9647f2a1184c26b8e808ef5f69e6f87017ee7f83
Swarnimrajsanu <149811757+Swarnimrajsanu@users.noreply.github.com>
2026-02-08 02:29:36 +0530

    feat: add task retrieval and submission handling in worker router

--- backend/src/routers/worker.ts
+++ backend/src/routers/worker.ts
@@ -2,14 +2,90 @@ import { PrismaClient } from '@prisma/client';
 import "dotenv/config";
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
-import { worker } from 'node:cluster';
-import user from './user.js';
+import { getNextTask } from '../db.js';
+import { workerMiddleware } from '../middleware.js';
+import { createSubmissionInput } from '../types.js';
+const TOTAL_SUBMISSIONS = 100;
 
 const JWT_SECRET = "billubadmosh"
 const router = Router();
 
 export const WORKER_JWT_SECRET = JWT_SECRET + "worker";
 const prisma = new PrismaClient({});
+
+router.post("/submission", workerMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId = Number(req.userId);
+    const body = req.body;
+    const parsedBody = createSubmissionInput.safeParse(body);
+
+    if (parsedBody.success) {
+        const task = await getNextTask(Number(userId));
+        if (!task || task?.id !== Number(parsedBody.data.taskId)) {
+            return res.status(411).json({
+                message: "Incorrect task id"
+            })
+        }
+
+        const amount = (Number(task.amount) / TOTAL_SUBMISSIONS).toString();
+
+        const submission = await prisma.$transaction(async tx => {
+            const submission = await tx.submission.create({
+                data: {
+                    option_id: Number(parsedBody.data.selection),
+                    worker_id: userId,
+                    task_id: Number(parsedBody.data.taskId),
+                    amount: Number(amount)
+                }
+            })
+
+            await tx.worker.update({
+                where: {
+                    id: userId,
+                },
+                data: {
+                    pending_amount: {
+                        increment: Number(amount)
+                    }
+                }
+            })
+
+            return submission;
+        })
+
+        const nextTask = await getNextTask(Number(userId));
+        res.json({
+            nextTask,
+            amount
+        })
+        
+
+    } else {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        })
+            
+    }
+
+})
+
+router.get("/nextTask", workerMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId: string = req.userId;
+
+    const task = await getNextTask(Number(userId));
+
+    if (!task) {
+        res.status(411).json({   
+            message: "No more tasks left for you to review"
+        })
+    } else {
+        res.json({   
+            task
+        })
+    }
+})
+
 
 
 router.post("/signin", async (req, res) => {
